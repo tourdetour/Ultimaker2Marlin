c724c21b8ba2a412b7576f0c1b3a797c0eeb1920
TinkerGnome <info.mike.brandt@gmx.de>
2015-06-21 12:48:57 +0200

    Do not supply ABS and UPET material settings for the UM2go.

--- Marlin/UltiLCD2_menu_material.cpp
+++ Marlin/UltiLCD2_menu_material.cpp
@@ -767,23 +767,7 @@ void lcd_material_reset_defaults()
     eeprom_write_word(EEPROM_MATERIAL_FLOW_OFFSET(0), 100);
     eeprom_write_float(EEPROM_MATERIAL_DIAMETER_OFFSET(0), 2.85);
 
-    strcpy_P(buffer, PSTR("ABS"));
-    eeprom_write_block(buffer, EEPROM_MATERIAL_NAME_OFFSET(1), 4);
-    eeprom_write_word(EEPROM_MATERIAL_TEMPERATURE_OFFSET(1), 260);
-    eeprom_write_word(EEPROM_MATERIAL_BED_TEMPERATURE_OFFSET(1), 90);
-    eeprom_write_byte(EEPROM_MATERIAL_FAN_SPEED_OFFSET(1), 50);
-    eeprom_write_word(EEPROM_MATERIAL_FLOW_OFFSET(1), 107);
-    eeprom_write_float(EEPROM_MATERIAL_DIAMETER_OFFSET(1), 2.85);
-
-    strcpy_P(buffer, PSTR("UPET"));
-    eeprom_write_block(buffer, EEPROM_MATERIAL_NAME_OFFSET(2), 5);
-    eeprom_write_word(EEPROM_MATERIAL_TEMPERATURE_OFFSET(2), 250);
-    eeprom_write_word(EEPROM_MATERIAL_BED_TEMPERATURE_OFFSET(2), 60);
-    eeprom_write_byte(EEPROM_MATERIAL_FAN_SPEED_OFFSET(2), 50);
-    eeprom_write_word(EEPROM_MATERIAL_FLOW_OFFSET(2), 100);
-    eeprom_write_float(EEPROM_MATERIAL_DIAMETER_OFFSET(2), 2.85);
-
-    eeprom_write_byte(EEPROM_MATERIAL_COUNT_OFFSET(), 3);
+    eeprom_write_byte(EEPROM_MATERIAL_COUNT_OFFSET(), 1);
 }
 
 void lcd_material_set_material(uint8_t nr, uint8_t e)
